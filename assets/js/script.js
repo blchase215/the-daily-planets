68432be2fd49b6ce5c9887a40bea25ed27777674
Brian Chase <brianchasemusic@gmail.com>
2022-08-03 11:01:00 -0400

    save before merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ $( document ).ready(function() {
         var fromdate = $(this).val();
         alert(fromdate);
     });
-}); 
+})
 
 
 
@@ -102,51 +102,20 @@ $( document ).ready(function() {
 var newsApiKey = '7in8TwtqzrrthvlHglN5nTxws1VNhYaztWPyp3ih';
 var newsQueryString = 'https://api.thenewsapi.com/v1/news/top?api_token=' + newsApiKey;
 var marsApiKey = 'psz2c1wYY3t9M2AONzlvkrwmbzmet6Gyv2NrfVQX';
-var marsQueryString = 'https://api.nasa.gov/planetary/apod?api_key=' + marsApiKey;
-var marsDateString = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key='+ marsApiKey + '&earth_date=2015-6-3';
+var marsQueryString = 'https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos?api_key=' + marsApiKey;
+var marsDateString = 'https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos?api_key='+ marsApiKey + '&earth_date=' + userDate; // 
 
 // console.log(newsQueryString);
 console.log(marsQueryString);
 console.log(marsDateString);
 
-// var citySearchInput = function(event) {
-//     event.preventDefault();
-//     //makes the city query parameter in the openweathmap API equal to the userinput
-//     city = searchInput.val();
-//     var queryURL =“https://api.openweathermap.org/data/2.5/weather?q=” + city + “&appid=” + APIkey;
-//    console.log(queryURL);
-//     //using AJAX and jquery to make this less painful
-//     $.ajax({
-//         url: queryURL,
-//         method: ‘GET’,
-//       }).then(function (response) {
-//         console.log(response);
-//         //math responsible for making temp in Farenheit
-//         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-//         //setting component of todays weather in city header card
-//         cityName.html(response.name);
-//         var icon = response.weather[0].icon;
-//         var iconURL = “https://openweathermap.org/img/wn/”+icon+“.png”;
-//         $(‘.todays-weather’).html(“<img src=“+iconURL+“>”);
-//         $(‘.todays-temp’).html(‘Temp(F): ’ + (tempF).toFixed(1));
-//         $(‘.todays-wind’).html(‘Wind: ’ + response.wind.speed + ‘mph’ );
-//         $(‘.todays-humidity’).html(‘Humidity: ’ + response.main.humidity);
-//         //creating a button capable of performing cityDataPopulate() to redo most of this function when pressed
-//         var newCityBtn = $(‘<btn>‘+ city + ‘</btn>’);
-//         newCityBtn.attr(‘class’, ‘city-btn’);
-//         newCityBtn.attr(‘type’, ‘button’);
-//         newCityBtn.attr(‘style’, ‘text-align:center’)
-//         userCities.append(newCityBtn);
-//         //run code to fetch the data to populate the five forecast cards in a nested function
-//         cityForecast(response.id);
-//         //UVIndex is called here using onecall API. Lat and Long are pulled from this API response. This single API response is responsible for two other API responses.
-//         UVIndex(response.coord.lon, response.coord.lat);
-//         //set Store the city name in a number starting at one and incrementing each time this function is called until page is refreshed.
-//         keyNum++;
-//         console.log(keyNum);
-//         localStorage.setItem(keyNum, city);
-//       });
-// };
+// builds search query from date provided
+// currently just curiosity
+// date needs to be a string, with format "yyyy-m-d" (no 0 before single digits)
+function marsQuery(date) {
+    marsQueryString
+}
+
 
 
 
